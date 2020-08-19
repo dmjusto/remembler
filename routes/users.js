f641936adbe82152c5f7f1bb51e7ae925869ea7e
dmjusto <31903571+dmjusto@users.noreply.github.com>
2020-08-18 18:12:27 -0600

    add check for existing user and password encryption to register user route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/userModel');
 
 // @route Post /api/users
 // @desc Register User
 // @access Public
 router.post(
     '/',
-    //Input Validation
+    // Input Validation
     [
         check('email', 'Valid email is required').isEmail(),
         check(
@@ -15,13 +18,41 @@ router.post(
             'Please enter a password with 6 or more characters'
         ).isLength({ min: 6 }),
     ],
-    (req, res) => {
+    async (req, res) => {
+        // Check for Errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        res.send('User Route');
+        const { email, password } = req.body;
+
+        try {
+            // Check if user is already registered
+            let user = await User.findOne({ email });
+            if (user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'User already exists' }] });
+            }
+
+            // Create new user instance
+            user = new User({
+                email,
+                password,
+            });
+
+            // Encrypt password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            res.send('User Registered');
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
     }
 );
 

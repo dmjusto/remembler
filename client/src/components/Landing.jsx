8145ef58f57c5522cd903f7db0863a959e0a1fba
dmjusto <31903571+dmjusto@users.noreply.github.com>
2020-08-26 15:40:03 -0600

    style landing page

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,13 +1,42 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import './Landing.css';
 
 const Landing = () => {
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            '& > *': {
+                margin: theme.spacing(1),
+            },
+        },
+    }));
+
+    const classes = useStyles();
+
     return (
-        <Fragment>
+        <Container>
             <section className='Landing'>
-                <div className='darkOverlay'></div>
+                <div className='landing-content'>
+                    <h1>Remembler</h1>
+                    <h3>Create and Share Flashcard Decks</h3>
+                    <div className={classes.root}>
+                        <Button variant='contained' color='primary'>
+                            <Link className='action-links' to='/register'>
+                                Register
+                            </Link>
+                        </Button>
+                        <Button variant='contained' color='secondary'>
+                            <Link className='action-links' to='/login'>
+                                login
+                            </Link>
+                        </Button>
+                    </div>
+                </div>
             </section>
-        </Fragment>
+        </Container>
     );
 };
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Hidden from '@material-ui/core/Hidden';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import './Navbar.css';
 
 const Navbar = () => {
+    const [anchorEl, setanchorEl] = React.useState(null);
+
+    const handleClick = (e) => {
+        setanchorEl(e.currentTarget);
+    };
+
+    const handleClose = (e) => {
+        setanchorEl(null);
+    };
     return (
         <nav className='Navbar'>
+            <Hidden smUp>
+                <div>
+                    <Button
+                        aria-controls='simple-menu'
+                        aria-haspopup='true'
+                        onClick={handleClick}
+                    >
+                        <MenuIcon className='menuIcon' />
+                    </Button>
+                    <Menu
+                        id='simple-menu'
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>
+                            <Link className='menuLinks' to='/register'>
+                                Register
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={handleClose}>
+                            <Link className='menuLinks' to='/login'>
+                                Login
+                            </Link>
+                        </MenuItem>
+                    </Menu>
+                </div>
+            </Hidden>
+
             <Link to='/'>
                 <h1>Remembler</h1>
             </Link>
+
             <div className='links'>
-                <Link to='/register'>Register</Link>
-                <Link to='/login'>Login</Link>
+                <Hidden xsDown>
+                    <Link className='navLinks' to='/register'>
+                        Register
+                    </Link>
+                    <Link className='navLinks' to='/login'>
+                        Login
+                    </Link>
+                </Hidden>
             </div>
         </nav>
     );
